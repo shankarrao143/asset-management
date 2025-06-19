529efa63e9c2b7d658672c69502308761b255cd6
shankar <sankarrao.c123@gmail.com>
2025-06-19 17:19:10 +0530

    file2

---
+++ infra/variable.tf
@@ -0,0 +1,15 @@
+variable "amiid"{
+    default = ""
+}
+
+variable "region"{
+    default = "ap-south-1"
+}
+
+variable "instancetype"{
+    default = "t2.micro"
+}
+
+variable "instancename"{
+    default = "hello-project"
+}
